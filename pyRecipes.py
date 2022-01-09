ef8fadb840302174938e3d5e2a8d8e8ea04e8a38
koter <42820377+vbaaccess@users.noreply.github.com>
2022-01-09 22:17:53 +0100

    Update pyRecipes.py

--- pyRecipes.py
+++ pyRecipes.py
@@ -1,2 +1,23 @@
-def get_recepie_links():
-    pass
+from typing import Set
+import requests
+from bs4 import BeautifulSoup
+import re
+
+def get_recepie_links(keyword: str) -> Set[str]:
+    """
+    Extracting links from search engine with recipes.
+    """
+    recepie_links = set()
+    url ='https://www.kwestiasmaku.com'
+    adres = url +'/szukaj?search_api_views_fulltext={}'.format(keyword)
+    response = requests.get(adres)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    div = soup.select('div[class*="view-wyniki-wyszukiwania"]')
+
+    divs = div[0].find_all('div', class_="field field-name-title field-type-ds field-label-hidden")
+    for div in divs:
+        result = re.search('"/(.*)">', str(div))
+        recepie_links.add(url + '/' + result.group(1))
+
+    return recepie_links
